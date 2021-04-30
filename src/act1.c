ba57fabbbc7e8d931be3943682c422d6bda2b4a3
280439 <82118144+280439@users.noreply.github.com>
2021-04-30 19:16:04 +0530

    Update act1.c

--- src/act1.c
+++ src/act1.c
@@ -17,24 +17,24 @@
 void act1()
 {
     DDRB |= (1<<PB0);//set the bit PB0 
-    DDRD &= ~(1<<PD0);//clear the bit PD0 
-    DDRD &= ~(1<<PD1);//clear the bit PD1
-    PORTD |= (1<<PD0);
-    PORTD |= (1<<PD1);
+    DDRC &= ~(1<<PC0);//clear the bit PD0 
+    DDRD &= ~(1<<PD0);//clear the bit PD1
+    PORTB |= (1<<PB0);
+    PORTC |= (1<<PC0);
 
-            if(((PIND & (1<<PD0))) && ((PIND & (1<<PD1))))
+            if(((PINB & (1<<PB0))) && ((PINC & (1<<PC0))))
             {
                 PORTB &= ~(1<<PB0);
             }
-            else if(((PIND & (1<<PD0))) && (!(PIND & (1<<PD1))))
+            else if(((PINB & (1<<PB0))) && (!(PINC & (1<<PC0))))
             {
                 PORTB &= ~(1<<PB0);
             }
-            else if((!(PIND & (1<<PD0))) && ((PIND & (1<<PD1))))
+            else if((!(PINB & (1<<PB0))) && ((PINC & (1<<PC0))))
             {
                 PORTB &= ~(1<<PB0);
             }
-            else if((!(PIND & (1<<PD0))) && (!(PIND & (1<<PD1))))
+            else if((!(PINB & (1<<PB0))) && (!(PINC & (1<<PC0))))
             {
                 PORTB |= (1<<PB0);
             }
